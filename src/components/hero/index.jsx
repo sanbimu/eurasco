003563cd52de9fb2d9445e7d385c4a1aefd5e915
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-07-07 11:57:10 +0200

    Installed Prismic

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -3,7 +3,12 @@ export const Hero = () => {
     <>
       <div className="md:hidden flex">
         <img src="/images/home.jpg" alt="Home"></img>
+        <div className="absolute z-10 flex flex-col self-center">
+          <h2 className="uppercase">A PROFESSION OF HOPE</h2>
+          <h1 className="uppercase">THE PURE NATURALLY HEALING</h1>
+        </div>
       </div>
+
       <div className="hidden md:flex">
         <img
           src="/images/homebig.jpg"
