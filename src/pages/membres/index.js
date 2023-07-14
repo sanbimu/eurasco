e0a7be2627b8cff2654692cdd84b5e0bec30da82
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-07-14 14:33:10 +0200

    member page mobile done

--- src/pages/membres/index.js
+++ src/pages/membres/index.js
@@ -1,12 +1,32 @@
+import { MemberCard } from "@/components/members/memberCard";
+import { CardAll } from "@/components/shared/cardAll";
+import { ContactCard } from "@/components/shared/contactCard";
+import { HeaderPages } from "@/components/shared/headerPages";
+import { Title } from "@/components/shared/title";
+
 export default function Members() {
   return (
     <main>
-      <div className="flex flex-col">
-        <div className="md:hidden flex h-screen">
-          <img src="/images/homeMobile.jpg" alt="Home"></img>
-        </div>
-        <div className="hidden lg:flex h-screen">
-          <img src="/images/homeDesktop.jpg" alt="Home"></img>
+      <div className="flex flex-col md:h-screen lg:h-auto">
+        <HeaderPages title="Nos membres" />
+        <div className="flex flex-col mt-12">
+          <Title
+            title="rencontrez"
+            subtitle="nos membres"
+            text="We have a team of professionals, dedicated to providing excellent service."
+          />
+          <div className="flex flex-col gap-4 mb-12">
+            <MemberCard />
+            <MemberCard />
+            <CardAll
+              title="Tous nos membres"
+              buttonText="voir tout"
+              linkTo={"/membres"}
+            />
+            <MemberCard />
+            <MemberCard />
+          </div>
+          <ContactCard />
         </div>
       </div>
     </main>
