b899aed30ab844be2a3c6c2e835138d004434c93
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-08-17 16:02:17 +0200

    event UID

--- src/pages/events/[uid].jsx
+++ src/pages/events/[uid].jsx
@@ -41,7 +41,7 @@ export default function EventPage({ eventPage, homePage, members }) {
         />
         <div className="absolute inset-0 gradient_Event z-10"></div>
         <div className="flex flex-col items-start justify-end min-h-[75vh] p-[25px] gap-4 md:gap-10 z-20">
-          <div className="font-mont font-bold text-3xl md:text-6xl leading-8 text-white md:mx-20 ">
+          <div className="font-mont font-bold text-3xl md:text-6xl lg:text-5xl leading-8 text-white md:mx-20 ">
             {eventPage.data.name}
           </div>
           <div className="font-mont font-semibold text-opacity-60 text-xl md:text-3xl leading-6 text-white md:mx-20 md:pb-10">
@@ -54,7 +54,8 @@ export default function EventPage({ eventPage, homePage, members }) {
       <div className="mb-12"></div>
 
       {/* CONTENT */}
-      <div className="flex flex-col font-open leading-6 md:px-0 lg:w-[50%] text-black pb-6">
+      {/* ----MOBILE---- */}
+      <div className="flex lg:hidden flex-col font-open leading-6 md:px-0 lg:w-[50%] text-black pb-6">
         <div className="font-mont font-bold text-lg md:text-xl leading-6 mx-5 md:mx-20 ">
           Organised by:&nbsp;
           <Link href={eventPage.data.member.url}>
@@ -87,7 +88,7 @@ export default function EventPage({ eventPage, homePage, members }) {
           <Image
             src={eventPage.data.logo.url}
             fill={true}
-            alt="Event Image"
+            alt="Event Logo"
             className="object-contain "
           />
         </div>
@@ -103,45 +104,104 @@ export default function EventPage({ eventPage, homePage, members }) {
           />
         </div>
       </div>
+
+      {/* ----DESKTOP---- */}
+      <div className="hidden lg:flex max-h-[20vh] max-w-full mx-auto my-12 justify-center">
+        <Image
+          src={eventPage.data.logo.url}
+          height={450}
+          width={450}
+          alt="Event Logo"
+          className="object-contain"
+        />
+      </div>
+      <div className="hidden lg:flex flex-row mb-12 mx-24 border md:border-lightGrey/10 md:rounded-[20px] md:bg-lightGrey md:bg-opacity-10 justify-evenly">
+        <div className="flex w-[45%] relative overflow-hidden my-12 rounded-[10px]">
+          <Image
+            src={eventPage.data.imageEvent.url}
+            height={450}
+            width={600}
+            alt="Event Image"
+            className="max-w-full"
+          />
+        </div>
+        <div className="flex flex-col w-[45%] my-12 gap-6">
+          <div className="font-mont font-bold text-lg leading-6">
+            Organised by:&nbsp;
+            <Link href={eventPage.data.member.url}>
+              {matchingMember.data.name}
+            </Link>
+            {matchingMemberTwo && (
+              <>
+                ,&nbsp;
+                <Link
+                  href={eventPage.data.member.url}
+                  className="font-mont font-bold text-lg leading-6"
+                >
+                  {matchingMemberTwo.data.name}
+                </Link>
+              </>
+            )}
+            {matchingMemberThree && (
+              <>
+                ,&nbsp;
+                <Link
+                  href={eventPage.data.member.url}
+                  className="font-mont font-bold text-lg leading-6"
+                >
+                  {matchingMemberThree.data.name}
+                </Link>
+              </>
+            )}
+          </div>
+          <div className="font-open leading-[25px] text-justify">
+            <PrismicRichText field={eventPage.data.description} />
+          </div>
+        </div>
+      </div>
+
       {/* INFO & BUTTONS */}
-      <div className="flex flex-col gap-6 my-6 ">
-        <ButtonInfo
-          paddingTB="2"
-          marginTB="0"
-          linkTo={eventPage.data.website.url}
-          icon="/icons/website.svg"
-          iconHeight="[55px]"
-          fontSize="[17px]"
-          text="GO TO WEBSITE"
-        />
-        <ButtonInfo
-          paddingTB="4"
-          marginTB="0"
-          linkTo={`http://maps.google.com/?q=${eventPage.data.city}`}
-          icon="/icons/address.svg"
-          iconHeight="[40px]"
-          fontSize="[15px]"
-          text={`${eventPage.data.city}, ${eventPage.data.country}`}
-        />
-        <ButtonInfo
-          paddingTB="4"
-          marginTB="0"
-          linkTo={`mailto:${eventPage.data.mail}`}
-          icon="/icons/mail.svg"
-          iconHeight="[40px]"
-          fontSize="[15px]"
-          text={eventPage.data.mail}
-        />
-        <ButtonInfo
-          paddingTB="4"
-          marginTB="0"
-          linkTo={`tel:${eventPage.data.phone}`}
-          icon="/icons/phone.svg"
-          iconHeight="[40px]"
-          fontSize="[15px]"
-          text={eventPage.data.phone}
-        />
-        <div className="mb-4"></div>
+      <div className="flex flex-col gap-6 mt-6 mb-12 mx-5 md:mx-20 lg:mx-2 lg:justify-center lg:flex-row">
+        <div className="flex flex-col gap-6 lg:w-[30vw]">
+          <ButtonInfo
+            paddingTB="2"
+            marginTB="0"
+            linkTo={eventPage.data.website.url}
+            icon="/icons/website.svg"
+            iconHeight="[45px]"
+            fontSize="[17px]"
+            text="GO TO WEBSITE"
+          />
+          <ButtonInfo
+            paddingTB="4"
+            marginTB="0"
+            linkTo={`http://maps.google.com/?q=${eventPage.data.city}`}
+            icon="/icons/address.svg"
+            iconHeight="[40px]"
+            fontSize="[15px]"
+            text={`${eventPage.data.city}, ${eventPage.data.country}`}
+          />
+        </div>
+        <div className="flex flex-col gap-6  lg:w-[30vw]">
+          <ButtonInfo
+            paddingTB="4"
+            marginTB="0"
+            linkTo={`mailto:${eventPage.data.mail}`}
+            icon="/icons/mail.svg"
+            iconHeight="[40px]"
+            fontSize="[15px]"
+            text={eventPage.data.mail}
+          />
+          <ButtonInfo
+            paddingTB="4"
+            marginTB="0"
+            linkTo={`tel:${eventPage.data.phone}`}
+            icon="/icons/phone.svg"
+            iconHeight="[40px]"
+            fontSize="[15px]"
+            text={eventPage.data.phone}
+          />
+        </div>
       </div>
     </>
   );
