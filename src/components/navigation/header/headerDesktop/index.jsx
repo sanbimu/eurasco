0f06fb9233352ebf67eb92d9dcc180de901ee15c
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-08-21 15:27:16 +0200

    test Router

--- src/components/navigation/header/headerDesktop/index.jsx
+++ src/components/navigation/header/headerDesktop/index.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import { navLinks } from "../../navLinks";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const HeaderDesktop = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [logoSize, setLogoSize] = useState({ width: 160, height: 100 });
-  const currentPath =
-    typeof window !== "undefined" ? window.location.pathname : "/";
-  const isContactPage = currentPath === "/contact";
+  const router = useRouter();
+  const isContactPage = router.pathname === "/contact";
 
   useEffect(() => {
     const handleScroll = () => {
@@ -56,9 +56,10 @@ const HeaderDesktop = () => {
         <div className="flex gap-16 uppercase font-open text-white items-center text-[17px]">
           {navLinks.map((link, index) => {
             const isActive =
-              currentPath === link.path ||
-              currentPath.startsWith(`${link.path}/`);
+              router.pathname === link.path ||
+              router.pathname.startsWith(`${link.path}/`);
             const isLast = index === navLinks.length - 1;
+            console.log(isActive);
             return (
               <Link
                 key={link.name}
